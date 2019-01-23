0f2d9e1924d978f8de28ab6ecad74f24f2204cbe
Azim212 <183574a@mymail.nyp.edu.sg>
2019-01-23 21:57:35 +0800

    integration with Kovi. IDK how/where to access josh's parts. Please link up all your pages one way or the other

--- Test.py
+++ Test.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_babel import *
-import shelve
+from simData import *
 from Record import Record
 from AddRecordForm import *
+import shelve
+import AdaptedSimulationCode as simCode
+
 
 app = Flask(__name__)
 app.config['BABEL_DEFAULT_LOCALE'] = 'en'
@@ -16,7 +19,7 @@ def get_locale():
 
 @app.route("/")
 def main():
-    return render_template("sportshome.html")
+    return render_template("SmartLivingHomepage.html")
 
 
 @app.route("/sportshome")
@@ -76,6 +79,70 @@ def summary():
         list.append(item)
     return render_template('summary.html', records=list, count=len(list))
 
+
+@app.route('/Sim.html', methods=['GET', 'POST'])
+def sim():
+    calc = simData(request.form)
+    if request.method == 'POST':
+            if calc.validate():
+                with shelve.open('simStorage') as simStorage:
+                    simStorage['ledNum'] = int(calc.led.data)
+                    simStorage['cflNum'] = int(calc.cfl.data)
+                    simStorage['incNum'] = int(calc.inc.data)
+                    simStorage['toiletNum'] = int(calc.toish.data)
+                    simStorage['toiletType'] = calc.toitype.data
+                    led = simStorage['ledNum']
+                    cfl = simStorage['cflNum']
+                    inc = simStorage['incNum']
+                    toitype = simStorage['toiletType']
+                finalWatt = simCode.calcWatt()
+                finalPrice = simCode.calcWattPrice()
+                dailyWatt = round(finalWatt / 30, 2)
+                dailyPrice = round(finalPrice / 30, 2)
+                yearlyWatt = round(finalWatt * 12, 2)
+                yearlyPrice = round(finalPrice * 12, 2)
+                cubmtrperday = simCode.calcCubmtr()
+                cubmtrPrice = simCode.calcCubmtrPrice()
+                dailyCubmtr = round(cubmtrperday / 30, 2)
+                dailyCubmtrPrice = round(cubmtrPrice / 30, 2)
+                yearlyCubmtr = round(cubmtrperday * 12, 2)
+                yearlyCubmtrPrice = round(cubmtrPrice * 12, 2)
+                tipElc = simCode.tipsElc()
+                tipWtr = simCode.tipsWtr()
+                global replaceInc
+                global replaceCfl
+                global saveSmartE
+                global saveSmartW
+                global replaceOldorConv
+                replaceInc = False
+                replaceCfl = False
+                saveSmartE = False
+                saveSmartW = False
+                replaceOldorConv = False
+                for i in tipElc:
+                    if i == 'replaceInc':
+                        replaceInc = True
+                    if i == 'replaceCfl':
+                        replaceCfl = True
+                    if i == 'saveSmartE':
+                        saveSmartE = True
+                for i in tipWtr:
+                    if i == 'replaceOldorConv':
+                        replaceOldorConv = True
+                    if i == 'saveSmartW':
+                        saveSmartW = True
+                openTab = True
+                return render_template("Sim.html", toitype=toitype, inc=inc, cfl=cfl, led=led, replaceOldorConv=replaceOldorConv, saveSmartW=saveSmartW, saveSmartE=saveSmartE, replaceCfl=replaceCfl, replaceInc=replaceInc, openTab=openTab, cubmtrPrice=cubmtrPrice, cubmtrperday=cubmtrperday,
+                                       yearlyCubmtrPrice=yearlyCubmtrPrice, yearlyCubmtr=yearlyCubmtr,
+                                       dailyCubmtrPrice=dailyCubmtrPrice, dailyCubmtr=dailyCubmtr,
+                                       yearlyPrice=yearlyPrice, yearlyWatt=yearlyWatt, dailyPrice=dailyPrice,
+                                       dailyWatt=dailyWatt, finalPrice=finalPrice, finalWatt=finalWatt,
+                                       calc=calc)
+            else:
+                error = 'Only numbers lower than 100 are allowed.'
+                return render_template("Sim.html", error=error, calc=calc)
+    else:
+        return render_template("Sim.html", calc=calc)
 
 if __name__ == "__main__":
     app.run(debug=True)
